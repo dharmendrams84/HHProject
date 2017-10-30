bd9986d5c08847a1601d62dbf790554f29433fe4
Dharmendra Mohapatra <dharmendra.m@ignitiv.com>
2017-10-30 16:23:35 +0530

    commting

---
+++ src/main/java/com/homehardware/Employee.java
@@ -0,0 +1,72 @@
+package com.homehardware;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "EMPLOYEE")
+public final class Employee implements Serializable {
+
+	@Id
+	@Column(name = "ID")
+	//@GeneratedValue
+	private Integer id;
+     
+	@Column(name = "FIRSTNAME")
+	private String firstname;
+ 
+	@Column(name = "LASTNAME")
+	private String lastname;
+ 
+	@Column(name = "EMAIL")
+	private String email;
+
+	@Column(name = "TELEPHONE")
+	private String telephone;
+     
+     
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public void setTelephone(final String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setFirstname(final String firstname) {
+		this.firstname = firstname;
+	}
+
+	public void setLastname(final String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+}
--- src/main/java/com/homehardware/HhController.java
+++ src/main/java/com/homehardware/HhController.java
@@ -1,6 +1,9 @@
 package com.homehardware;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public final class HhController {
 	protected static final Logger logger = Logger.getLogger(HhController.class);
 
+	@Autowired
+    private SessionFactory sessionFactory;
+	
+	/*@Autowired
+    private Employee employee;
+	*/
 	@RequestMapping("/login")
 	public final String login() {
 		logger.error("inside login method");
-		return "home.html";
+		final Session session = sessionFactory.openSession();
+		final Employee employee = session.load(Employee.class,1);
+		logger.error("after persisting employee "+employee.getEmail());
+		return "home";
 	}
 }
